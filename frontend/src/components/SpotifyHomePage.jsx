13281ef9a6b020e36ddfa1803bd22470ff7e4002
Aaron Jacob <aaronjacobaj4@gmail.com>
2024-02-16 12:41:37 +0530

    fixed centering issues

--- frontend/src/components/SpotifyHomePage.jsx
+++ frontend/src/components/SpotifyHomePage.jsx
@@ -13,12 +13,12 @@ const SpotifyHomePage = () => {
       <h2 className="text-[1.303rem] font-inherit mt-8 text-white outfit-font mq750:gap-[5.88rem] mq1025:gap-[5.88rem] mb-7">latest on <img src={spotify2} alt="Spotify Icon" className="inline-block h-[1.13rem] w-[1.13rem]" /></h2>
 
       {latestRelease && (
-        <div className="mx-auto mt-4 p-4 max-w-sif" style={{ backgroundColor: 'rgba(196, 51, 52)', borderRadius: '12px', overflow: 'hidden' }}>
+        <div className="mx-auto mt-4 p-5 max-w-sif" style={{ backgroundColor: 'rgba(196, 51, 52)', borderRadius: '12px', overflow: 'hidden' }}>
           <iframe 
             style={{ borderRadius: '12px' }}
             src={latestRelease.spotifyLink}
             width="100%" 
-            height="200" 
+            height="155" 
             frameBorder="0" 
             allowFullScreen
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
