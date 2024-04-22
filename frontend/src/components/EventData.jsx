c89e0da77f3f9a4f8e0c44a81a7497d60bb13fef
navadeep satheesh <navadeepsatheesh@gmail.com>
2024-04-22 23:18:21 +0530

    events page added

---
+++ frontend/src/components/EventData.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function EventData({width, showRegisterButton , details}) {
+  return (
+
+    <div className="events_data" style= {{width : width}}>
+    <div className="events_data_main">
+      <div className="events_data_poster"></div>
+      <div className="events_data_details">
+        <h3 className='upcoming_event_title'>{details[0]}</h3>
+        <p className="events_data_desc">
+          Lorem ipsum dolor sit amet consectetur. Posuere quam suspendisse sapien non nulla ultrices. Id semper malesuada adipiscing aliquam magna turpis. Turpis vestibulum diam pellentesque eu dictum. Faucibus fusce nullam nulla quis.
+        </p>
+        {
+          showRegisterButton && <button className="events_data_register">Register</button>
+        }
+        
+      </div>
+    </div>
+
+  </div>
+   
+  )
+}
--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,15 +1,120 @@
 // Events.jsx
 
-import React from 'react';
+import React , {useState , useEffect} from 'react';
 import Header from '../components/Header';
+
+import EventData from '../components/EventData';
+import Footer from '../components/Footer';
+
+import "../styles/events.css"
+
 
 const Events = () => {
   // Your Events component logic
+
+  var [events, setEvents] = useState( [
+    ['event1' , 'https://preview.redd.it/i-got-bored-so-i-decided-to-draw-a-random-image-on-the-v0-4ig97vv85vjb1.png?width=640&crop=smart&auto=webp&s=22ed6cc79cba3013b84967f32726d087e539b699'],
+    ['event2', 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg'],
+    ['event3' , 'https://lh3.googleusercontent.com/hwau7OVWx96XaME5KpRuJ0I_MscrerK6SbRH1UwYHYaxIDQQtn7RZK02LDSfBzCreidFgDsJeXyqDct6EZiH6vsV=w640-h400-e365-rj-sc0x00ffffff'],
+
+    ["event4" , "https://repository-images.githubusercontent.com/260096455/47f1b200-8b2e-11ea-8fa1-ab106189aeb0"]
+  ])
+
+  // events = events.reverse()
+
+  const cardCount = 3;
+
+  var [ positions , setPositions] = useState( Array.from({length: events.length}, (x, i) => i ))
+
+
+  var [ cards , setCards] = useState( Array.from({length: cardCount}, (x, i) => i ))
+
+
+  const changeEvent = ()=>{
+
+    cards = [...cards]
+
+    let c = cards.shift();
+    cards.push(c);  
+
+    positions = [...positions]
+    let p = positions.shift()
+    positions.push(p)
+
+    setCards(cards);
+    setPositions(positions)
+   
+    console.log(positions)
+
+  }
+
   return (
-    <div>
-    <Header />
-    <div className="mt-20"></div>
-      {/* Events page content */}
+    <div className="events" >
+      <Header />
+      <div className="events_container">
+
+
+       {/*  upcoming event section */}
+
+        <div className="upcoming_events">
+          <div className="events_data_heading"><h3 className='h_upcoming'>Upcoming</h3><h3 className='h_events'>EVENTS</h3></div>
+          <EventData showRegisterButton={true} details = {[ 'RJ HUNT' ]}/>
+        </div>
+
+
+        {/* past event section  */}
+
+        <div className="past_events">
+
+          {/*  displayed past event */}
+          <div className="past_events_container">
+
+            <div className="events_data_heading"><h3 className='h_upcoming'>Past</h3><h3 className='h_events'>EVENTS</h3></div>
+            <EventData className="single_past_event" width="80%"  details= {events[0 ]} />
+
+          </div>
+
+          {/*  past events card stack */}
+
+          <div className="past_event_switcher">
+
+            <div className="cards">
+
+              {
+                events.slice(0 , cardCount).reverse().map((event ,index)=>{
+
+                  return (
+
+                    <div 
+                        className="card1 card" 
+                        style={{
+                          right : cards[index]*20,
+                          zIndex: 1000+cards[ index],  
+                          height: `${85 + ( cards[index])*3}%`,
+                         
+                        }}
+
+                        onClick={changeEvent}
+                        >{events[ positions[index] ][0] }
+
+                        <img src= { events[ positions[index] ][1]  } alt="" />
+                    </div>
+                  )
+                  
+                })
+              }
+
+              
+            </div>
+
+          </div>
+
+        </div>
+
+
+      </div>
+
+      <Footer />
     </div>
   );
 };
