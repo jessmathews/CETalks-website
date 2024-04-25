0a541f715d925b99d3d3e6deace594806edb75dc
Jess <81862618+jessmathews@users.noreply.github.com>
2024-04-25 18:22:44 +0000

    Update banner and header styling

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,7 +10,9 @@ export default {
         gold: "#e8e034",
       },
       backgroundColor: {
-        '07060e': '#07060e',
+        '07060ef': '#07060e',
+        '07060e': '#100D0D',
+
       },
       backgroundImage: {
         'dust': "url('/images/Dust.png')",
