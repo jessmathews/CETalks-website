f0e8b7d088a11e2a9166a542e77727e07f3f4ea0
Jess <jessmathews190@gmail.com>
2024-11-10 23:37:23 +0530

    add info to carousel

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import euphony_western from '../assets/img/latest/euphony_western.jpg';
+import euphony_eastern from '../assets/img/latest/euphony_eastern.jpg';
 
 function Carousel() {
   var settings = {
@@ -11,27 +13,16 @@ function Carousel() {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 4000,
-    pauseOnHover: true
+    pauseOnHover: true,
+    outerWidth: false,
   };
   return (
     <Slider {...settings} >
       <div>
-        <img src="https://placehold.co/400X200" className="winner-image max-h-[15rem] mx-auto object-contain m-[1rem]"></img>
+        <img src={euphony_eastern} className="winner-image max-h-[15rem] mx-auto object-contain m-[1rem]"></img>
       </div>
       <div>
-        <img src="https://placehold.co/400X200" className="winner-image max-h-[15rem] mx-auto object-contain m-[1rem]"></img>
-      </div>
-      <div>
-        <img src="https://placehold.co/400X200" className="winner-image max-h-[15rem] mx-auto object-contain m-[1rem]"></img>
-      </div>
-      <div>
-        <img src="https://placehold.co/400X200" className="winner-image max-h-[15rem] mx-auto object-contain m-[1rem]"></img>
-      </div>
-      <div>
-        <img src="https://placehold.co/400X200" className="winner-image max-h-[15rem] mx-auto object-contain m-[1rem]"></img>
-      </div>
-      <div>
-        <img src="https://placehold.co/400X200" className="winner-image max-h-[15rem] mx-auto object-contain m-[1rem]"></img>
+        <img src={euphony_western} className="winner-image max-h-[15rem] mx-auto object-contain m-[1rem]"></img>
       </div>
     </Slider >
   );
